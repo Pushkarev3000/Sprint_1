72d3126b65f5d71c9822be9b23809788ffef1481
Pushkarev3000 <mailbox4user@yandex.ru>
2026-06-29 21:43:27 +0500

    убрал лишнее

--- task_1.py
+++ task_1.py
@@ -6,12 +6,8 @@ time_p_4 =[]
 time_p_5 =[]
 time_p_6 =[]
 time_sum_s = 0
-time_sum_m = 0
-a=1
-b=1
-for i in time_p:
-    i = i.split()
-    time_p_2.append(i)
+for item in time_p:
+    time_p_2.append(item.split())
 for sublist in time_p_2:
     for item in sublist:
         time_p_3.append(item.replace("h", " 3600"))
@@ -19,8 +15,8 @@ for item in time_p_3:
     time_p_4.append(item.replace("m", " 60"))
 for item in time_p_4:
     time_p_5.append(item.replace("s", " 1"))
-for time_split in time_p_5:
-    time_p_6.append(time_split.split())
+for item in time_p_5:
+    time_p_6.append(item.split())
 for circle in time_p_6:
     a = 0
     b = 1
